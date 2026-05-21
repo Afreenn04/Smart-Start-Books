fc0df05a650a961835984f51d279fcecc849af65
Afreen Fatima <32779588+Afreenn04@users.noreply.github.com>
2026-05-21 14:43:07 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router-dom";
-
 import App from "./App.jsx";
-import AdminDashboard from "./pages/AdminDashboard.jsx";
-import StudentDashboard from "./pages/StudentDashboard.jsx";
-import ParentDashboard from "./pages/ParentDashboard.jsx";
-
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/student" element={<StudentDashboard />} />
-        <Route path="/parent" element={<ParentDashboard />} />
-      </Routes>
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
