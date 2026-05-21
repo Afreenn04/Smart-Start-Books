ac4723281eeb03be453c64dd43597e1e47040416
Afreen Fatima <32779588+Afreenn04@users.noreply.github.com>
2026-05-21 14:23:59 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import App from "./App";
+import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
 import StudentDashboard from "./pages/StudentDashboard.jsx";
 import ParentDashboard from "./pages/ParentDashboard.jsx";
