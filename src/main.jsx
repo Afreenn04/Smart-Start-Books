231576d29b5301f224b9f9cc55326504090f52a3
Afreen Fatima <32779588+Afreenn04@users.noreply.github.com>
2026-05-22 21:23:50 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App.jsx";
+import EarlyLevel from "./pages/EarlyLevel.jsx";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/early-level" element={<EarlyLevel />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
